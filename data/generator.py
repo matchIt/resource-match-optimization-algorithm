d3e1e791b08962c12c69d7eeff6121bb5b8672ef
NanaKay007 <jnranikuabe@gmail.com>
2020-05-01 21:01:53 -0400

    added gitignore

---
+++ data/generator.py
@@ -0,0 +1,10 @@
+"""
+todo:
+create a random (but sensible) data generator:
+*should generate a number of health centers and generate randomized locations,
+random staff counts, carrying capitities, etc
+"""
+
+def generate_Random_LatLong_Within_RectArea(*args):
+    print()
+    pass
